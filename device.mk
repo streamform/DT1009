91105ddf46bc06f23dccbf692df9fe1d3d03b770
streamform <52741084+streamform@users.noreply.github.com>
2023-08-28 23:27:13 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -17,7 +17,7 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-     android.hardware.boot@1.1-impl.recovery
+     android.hardware.boot@1.1-impl
      #     android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
